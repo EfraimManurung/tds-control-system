536e2fe06a1d4f906d152a79583977c49da8b0f3
Efraim Manurung <frm199911@gmail.com>
2025-09-25 23:57:06 +0700

    first init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ *
+ * Total Dissolved Solids (TDS) Control System
+ *
+ * Author: Efraim Manurung
+ * Email: efraim.manurung@gmail.com
+ *
+ * Changelog:
+ *
+ * [01-09-2025]
+ *  - First initialization.
+ */
+
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Hi, from main app!");
+}
+
+void loop() { /* still empty for now */ }
